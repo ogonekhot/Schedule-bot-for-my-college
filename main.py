c449f3b8498a25812f700a7fa63576108651e5ad
LosharikYT <83444054+ogonekhot@users.noreply.github.com>
2025-11-22 14:52:36 +0300

    Обновлённая логика бота
    Более быстрая и надёжная логика. Я обновил почти всю логику, и добавил немного комментариев, буду стараться расписывать подробнее

--- main.py
+++ main.py
@@ -1,62 +1,33 @@
-from aiogram import types, Dispatcher, Bot, F
-from aiogram.filters import CommandObject, CommandStart
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.filters import CommandObject, CommandStart
 from aiogram.types import InlineKeyboardButton
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
-import arrow
+from aiogram import types, Dispatcher, Bot, F
+import moduls.time as tm
+import moduls.db as dbm
 import logging
 import asyncio
 import config
+import arrow
 import json
 
-##################################################################################################################
-
-moduls = 0
-
-try:
-    import moduls.db as dbm
-    moduls += 1
-except:
-    print('Ошибка загрузки db_module')
-
-# try:
-#     import moduls.schedule.schedule as sm
-#     moduls += 1
-# except:
-#     print('Ошибка загрузки schedule_module')
-
-# try:
-#     import moduls.schedule.schedule_manager as smn
-#     moduls += 1
-# except:
-#     print('Ошибка загрузки schedule_manager')
-
-try:
-    import moduls.time as tm
-    moduls += 1
-except:
-    print('Ошибка загрузки time_module')
-
-# try:
-#     import moduls.task as taskm
-#     moduls += 1
-# except:
-#     print('Ошибка загрузки task_module')
-
-print(f'Модулей установлено: {moduls}')
-
-##################################################################################################################
+#########################################################################################
+#                           Инициализация логирования и бота                            #
+#########################################################################################
 
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=config.TOKEN)
 dp = Dispatcher()
-scheduler = AsyncIOScheduler()
 
-admins = [134132306, 5207843376]
+#########################################################################################
+#                       Загрузка настроек и дополнительных файлов                       #
+#########################################################################################
 
-##################################################################################################################
+try:
+    with open(r"./settings/global.json", "r", encoding="utf_8_sig") as f:
+        settings = json.loads(f.read())
+except:
+    print('Не удалось загрузить глобальные настройки')
 
 try:
     with open(r"./settings/schedule.json", "r", encoding="utf_8_sig") as f:
@@ -70,185 +41,72 @@ try:
 except:
     print('Не удалось загрузить адреса')
 
-try:
-    with open(r"./settings/localization.json", "r", encoding="utf_8_sig") as f:
-        localization = json.loads(f.read())
-except:
-    print('Не удалось загрузить локализацию')
-
-##################################################################################################################
-
-@dp.message(CommandStart(deep_link=True))
-async def link(message: types.Message, command: CommandObject):
-    print("Рефералка")
+#########################################################################################
+#                                 Основная логика бота                                  #
+#########################################################################################
 
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    db_user_id = await dbm.check_user(message.from_user.id)
-    group = await dbm.settings_manager(db_user_id, 'check')
-
+@dp.callback_query(F.data == 'back')
+async def start(event: types.Message | types.CallbackQuery):
     buttons = InlineKeyboardBuilder()
-    if group != None and group != '-1':
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['schedule'], callback_data = f'schedule_{group}'))
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['alarms'], callback_data = f'alarms_{group}'))
-        text = localization['normal']['hello']['base']
-    elif group == '-1':
-        buttons.row(InlineKeyboardButton(text = 'Окак', callback_data = f'back'))
-        text = localization['normal']['hello']['terminate']
+    buttons.row(InlineKeyboardButton(text = 'Расписание', callback_data = f'schedule'))
+
+    if isinstance(event, types.Message):
+        message = event.answer
     else:
-        for group in schedule.keys():
-            buttons.row(InlineKeyboardButton(text = group, callback_data = f'alarms_{group}'))
-        text = localization['normal']['hello']['registarate']
+        message = event.message.edit_text
 
-    await message.answer(text, parse_mode = "Markdown", reply_markup = buttons.as_markup())
-
-@dp.callback_query(F.data[:6] == 'alarms')
-async def buttons_manager(callback: types.CallbackQuery):
-    db_user_id = await dbm.check_user(callback.from_user.id)
-
-    cs = str(callback.data).split('_')
-    buttons = InlineKeyboardBuilder()
-
-    if len(cs) == 2 and cs[1] != 'None':
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['yes'], callback_data = f'alarms_{cs[1]}_yes'))
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['no'], callback_data = f'alarms_{cs[1]}_no'))
-        text = localization['normal']['alarms']['quest']
-    elif len(cs) == 3:
-        if cs[2] == 'yes':
-            state = 1
-            text = localization['normal']['alarms']['ans_yes']
-        elif cs[2] == 'no':
-            state = 0
-            text = localization['normal']['alarms']['ans_no']
-
-        await dbm.settings_manager(db_user_id, 'add', cs[1], state)
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['ok'], callback_data = f'back'))
-
-    await callback.message.edit_text(text, parse_mode = "Markdown", reply_markup = buttons.as_markup())
+    await message('Добро пожаловать в бот расписания!', reply_markup=buttons.as_markup())
 
 @dp.callback_query(F.data[:8] == 'schedule')
-async def buttons_manager(callback: types.CallbackQuery):
-    db_user_id = await dbm.check_user(callback.from_user.id)
+async def schedule_manager(callback: types.CallbackQuery):
+    s = callback.data.split('_')
 
-    cs = str(callback.data).split('_')
-    buttons = InlineKeyboardBuilder()
-
-    if len(cs) == 2 and cs[1] != 'None':
-        if await tm.this_day(1) == 'ПН':
-            week = 'next'
-        else:
-            week = 'now'
-        
-        buttons.row(InlineKeyboardButton(text = '⏪', callback_data = f'schedule_{cs[1]}_back'), InlineKeyboardButton(text = 'Назад', callback_data = f'back'), InlineKeyboardButton(text = '⏩', callback_data = f'schedule_{cs[1]}_next'))
-
-        text = f'{await tm.this_day()} - {await tm.week_color(week)}\n\n'
-        for num in schedule[cs[1]][await tm.this_day()].keys():
-            room = str(schedule[cs[1]][await tm.this_day()][num][await tm.week_color('now')]['room'])
-            split_room = str(schedule[cs[1]][await tm.this_day()][num][await tm.week_color('now')]['room']).split('-')
-            try:
-                for housing in adresses.keys():
-                    for flor in adresses[housing]:
-                        if len(split_room) == 1:
-                            if int(room) > int(adresses[housing][flor]["min"]) and int(room) < int(adresses[housing][flor]["max"]):
-                                adres = housing
-                        else:
-                            if split_room[0] in housing:
-                                adres = housing
-            except:
-                adres = ''
-
-            text += f'{num}-я пара {schedule[cs[1]][await tm.this_day()][num]['time']['start']} - {schedule[cs[1]][await tm.this_day()][num]['time']['end']}\n{schedule[cs[1]][await tm.this_day()][num][await tm.week_color(week)]['title']}\n{schedule[cs[1]][await tm.this_day()][num][await tm.week_color(week)]['teacher']}\n{adres} {room} {schedule[cs[1]][await tm.this_day()][num][await tm.week_color(week)]['type']}\n\n'
-    else:
-        buttons.row(InlineKeyboardButton(text = 'Ок', callback_data = f'back'))
-        text = 'В разработке...'
-    await callback.message.edit_text(text, parse_mode = "Markdown", reply_markup = buttons.as_markup())
-
-@dp.callback_query(F.data == 'back')
-async def buttons_manager(callback: types.CallbackQuery):
-    db_user_id = await dbm.check_user(callback.from_user.id)
-    group = await dbm.settings_manager(db_user_id, 'check')
+    if len(s) == 1:
+        s.append(arrow.now().format("DD.MM.YYYY"))
 
     buttons = InlineKeyboardBuilder()
-    if group != None and group != '-1':
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['schedule'], callback_data = f'schedule_{group}'))
-        buttons.row(InlineKeyboardButton(text = localization['normal']['buttons']['alarms'], callback_data = f'alarms_{group}'))
-        text = localization['normal']['hello']['base']
-    elif group == '-1':
-        buttons.row(InlineKeyboardButton(text = 'Окак', callback_data = f'back'))
-        text = localization['normal']['hello']['terminate']
-    else:
-        for group in schedule.keys():
-            buttons.row(InlineKeyboardButton(text = group, callback_data = f'alarms_{group}'))
-        text = localization['normal']['hello']['registarate']
+    buttons.row(InlineKeyboardButton(text = '⏪', callback_data = f'schedule_{await tm.get_next_previous(s[1], "extra_previous")}'), InlineKeyboardButton(text = '◀️', callback_data = f'schedule_{await tm.get_next_previous(s[1], "previous")}'), InlineKeyboardButton(text = '🔄️', callback_data = f'schedule'), InlineKeyboardButton(text = '▶️', callback_data = f'schedule_{await tm.get_next_previous(s[1], "next")}'), InlineKeyboardButton(text = '⏩', callback_data = f'schedule_{await tm.get_next_previous(s[1], "extra_next")}'))
+    buttons.row(InlineKeyboardButton(text = 'Назад', callback_data = 'back'))
+    group = (await dbm.check_tg_id(callback.from_user.id))[1]
+    day, color = await tm.get_this_weekday(settings["references"]["date"], settings["references"]["color"], date=s[1])
+    
+    text = f'<b>{day} - {color} ({arrow.get(s[1], "DD.MM.YYYY").format("DD.MM")})</b>\n\n'
 
-    await callback.message.edit_text(text, parse_mode = "Markdown", reply_markup = buttons.as_markup())
-
-@dp.callback_query(F.data == 'ok')
-async def buttons_manager(callback: types.CallbackQuery):
-    await callback.message.edit_text('Не забудь!', parse_mode = "Markdown")
-    await asyncio.sleep(0.5)
-    await callback.message.delete()
-
-@dp.message()
-async def dell_user_massage(message: types.Message):
-    await message.delete()
-
-async def safe_daily_message(bot, group, num):
     try:
-        users = await dbm.users_on_alarms(group)
-        try:
-            room = str(schedule[group][await tm.this_day()][str(int(num) + 1)][await tm.week_color('now')]['room'])
-            split_room = str(schedule[group][await tm.this_day()][str(int(num) + 1)][await tm.week_color('now')]['room']).split('')
-            type = schedule[group][await tm.this_day()][str(int(num) + 1)][await tm.week_color('now')]['type']
-            work = schedule[group][await tm.this_day()][str(int(num) + 1)][await tm.week_color('now')]['title']
-            teach = schedule[group][await tm.this_day()][str(int(num) + 1)][await tm.week_color('now')]['teacher']
-        except Exception as e:
-            return
-        
-        adres = ''
+        schedule_fd = schedule[group][day]
+        count = 0
 
-        for housing in adresses.keys():
-            for flor in adresses[housing]:
-                if split_room[0] != 'Л':
-                    if int(room) > int(adresses[housing][flor]["min"]) and int(room) < int(adresses[housing][flor]["max"]):
-                        adres = housing
-                elif split_room[1] == '-':
-                    if split_room[0] in housing:
-                        adres = housing
+        for i in schedule_fd.keys():
+            if schedule_fd[i][color]["title"] != '' or schedule_fd[i][color]["teacher"] != '':
+                count += 1
+                text += f'{count}-я пара <b>{schedule_fd[i]["time"]["start"]} - {schedule_fd[i]["time"]["end"]}</b>:\n'
 
-        buttons = InlineKeyboardBuilder()
-        buttons.row(InlineKeyboardButton(text = 'Ок', callback_data = f'ok'))
-        # for user_id in users:
-        await bot.send_message(5207843376, f"{adres} {room} {type}\n{work}\n{teach}", parse_mode = "Markdown", reply_markup = buttons.as_markup())
-        scheduler.remove_job(f'alarm_{group}_{schedule[group][await tm.this_day()][str(int(num) + 1)]['time']['end']}')
+                if schedule_fd[i][color]["title"] != '':
+                    text += f'{schedule_fd[i][color]["title"]}\n'
 
-    except Exception as e:
-        print(e)
+                if schedule_fd[i][color]["teacher"] != '':
+                    text += f'{schedule_fd[i][color]["teacher"]}\n'
 
-async def setup_scheduler(bot):
-    for group in schedule.keys():
-        for num in schedule[group][await tm.this_day()].keys():
-            if num != len(schedule[group][await tm.this_day()]):
-                time = schedule[group][await tm.this_day()][num]['time']['end']
-                hour, minute = map(int, time.split(':'))
+                if schedule_fd[i][color]["room"] != '':
+                    for corpuse in adresses.keys():
+                        for flore in adresses[corpuse].keys():
+                            if str(schedule_fd[i][color]["room"]).isdigit() and str(schedule_fd[i][color]["room"]) != 'спортзал':
+                                if schedule_fd[i][color]["room"] >= adresses[corpuse][flore]["min"] and schedule_fd[i][color]["room"] <= adresses[corpuse][flore]["max"]:
+                                    adress = f'{corpuse} {schedule_fd[i][color]["room"]} {schedule_fd[i][color]["type"]}'
+                            elif str(schedule_fd[i][color]["room"])[0] == '9':
+                                adress = f'9-й корпус {str(schedule_fd[i][color]["room"])[2:]} {schedule_fd[i][color]["type"]}'
 
-                scheduler.add_job(
-                    safe_daily_message,
-                    CronTrigger(hour=hour, minute=minute),
-                    id=f'alarm_{group}_{time}',
-                    args=[bot, group, num]
-                )
+                    text += f'{adress}\n\n'
+    except:
+        text += 'Выходной'
+
+    try:
+        await callback.message.edit_text(text, reply_markup=buttons.as_markup(), parse_mode='HTML')
+    except:
+        pass
 
 async def main():
-    scheduler.add_job(
-        setup_scheduler,
-        CronTrigger(hour=8, minute=46),
-        id=f'alarms',
-        args=[bot]
-    )
-
-    await setup_scheduler(bot)
-    scheduler.start()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
